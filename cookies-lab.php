7e9c75348eea536a27f233231c9733b347ee9f73
Krzysztof <krzbracha@gmail.com>
2015-11-09 14:44:56 +0000

    phpmyadmin

--- cookies-lab.php
+++ cookies-lab.php
@@ -1,4 +1,10 @@
 <?php
+$servername = "csdm-mssql.rgu.ac.uk";
+$username = "1505499";
+$password = "password";
+
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if($_POST['login'] == "Mike"&& $_POST['password']="mysecretpassword")
         echo "good";
@@ -26,6 +32,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
     <label>Login: </label><input type="text" name="login" />
     <label>Password: </label><input type="password" name="password" />
+    <input type="submit" />
 </form>
 </body>
 </html>
